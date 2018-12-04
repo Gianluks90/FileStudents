a72d5a804bdf03f2539bdb135269b24e4559b2c2
Luca Lanaro <lanaroc@libero.it>
2018-12-04 11:34:26 +0100

    Aggiungere i file di progetto.

---
+++ FileStudents/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileStudents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
